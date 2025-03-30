8784277f8e7064d6c2b5ee099f7440747d2bdd8b
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-03-30 12:18:17 +0500

    export issue cleared

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -1,8 +1,15 @@
 import { createContext } from "react";
-import App from "../App";
 
-export const AppContext = createContext();
+// 1. First create the context
+const AppContext = createContext();
+
+// 2. Create the Provider component
 const AppContextProvider = (props) => {
   const value = {};
-  return <AppContext.Provider value ={value} >{props.children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
 };
+
+// 3. Properly export both the context and provider
+export { AppContext, AppContextProvider };
