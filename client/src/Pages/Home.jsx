a4cf498a6512d352ab702dfc2b59fc9ece71cb45
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-04-05 22:00:38 +0500

    side bar of job listig components

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import Joblisting from '../Components/Joblisting'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Joblisting/>
     </div>
   )
 }
